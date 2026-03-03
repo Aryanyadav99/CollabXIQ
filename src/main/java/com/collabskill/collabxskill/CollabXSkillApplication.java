efd4686c95c148ee19428afab8ccd43a4975e971
Aryanyadav99 <aryankietstudent@gmail.com>
2026-03-03 20:21:49 +0530

    Intilize Project

---
+++ src/main/java/com/collabskill/collabxskill/CollabXSkillApplication.java
@@ -0,0 +1,13 @@
+package com.collabskill.collabxskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabXSkillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollabXSkillApplication.class, args);
+    }
+
+}
